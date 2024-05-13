095d47b33fc339bb7a5dd99bbb37e3a038040393
magiicloud <hongyun_rc@outlook.com>
2024-05-13 22:09:58 +0800

    feat(user med): api to retrieve all the med details linked to user

--- src/app/api/medications/route.ts
+++ src/app/api/medications/route.ts
@@ -2,9 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 export const GET = async (req: NextRequest) => {
-  const drugs = await prisma.drug.findMany({
+  const drugs = await prisma.userDrug.findMany({
+    where: { userId: 1 },
     include: {
-      drugClasses: true,
+      drug: {
+        include: {
+          drugClasses: {
+            select: {
+              drugClass: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
     },
   });
   return NextResponse.json(drugs);
