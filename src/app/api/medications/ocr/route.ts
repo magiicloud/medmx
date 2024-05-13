2c740be989e4a937412f6f8254071eb37696484d
magiicloud <hongyun_rc@outlook.com>
2024-05-13 21:55:13 +0800

    feat(add med form): api to send image data to google document ai to return ocr data

--- src/app/api/medications/ocr/route.ts
+++ src/app/api/medications/ocr/route.ts
@@ -3,11 +3,21 @@ import { DocumentProcessorServiceClient } from "@google-cloud/documentai";
 import fs from "fs/promises";
 
 export const POST = async (req: NextRequest) => {
-  const filePath =
-    "/Users/hongyun/Rocket/bootcamp/m4/capstone-project/medmx/public/IMG_1316.jpeg";
+  // const file = process.env.SAMPLE_IMG_URL;
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
+  console.log(file);
+
+  if (!file) {
+    return NextResponse.json({
+      status: 400,
+      error: "Please select a file to be uploaded.",
+    });
+  }
 
   try {
-    const buffer = await fs.readFile(filePath);
+    // const buffer = await fs.readFile(file);
+    const buffer = Buffer.from(await file.arrayBuffer());
 
     // Convert the file to an ArrayBuffer then to a Buffer
     const encodedImage = buffer.toString("base64");
@@ -42,11 +52,11 @@ export const POST = async (req: NextRequest) => {
         switch (entity.type) {
           case "drug_name":
             extractedMedLabel.drugName =
-              entity.mentionText?.toLocaleLowerCase() ?? "";
+              entity.mentionText?.toUpperCase() ?? "";
             break;
           case "dosing_instruction":
             extractedMedLabel.dosingInstruction =
-              entity.mentionText?.toLocaleLowerCase() ?? "";
+              entity.mentionText?.toUpperCase() ?? "";
             break;
         }
       }
@@ -65,46 +75,3 @@ export const POST = async (req: NextRequest) => {
     });
   }
 };
-
-//ACTUAL CODE
-// export const POST = async (req: NextRequest) => {
-//   const formData = await req.formData();
-//   const file = formData.get("file") as File;
-//   if (!file) {
-//     return NextResponse.json({ Message: "No file received.", status: 400 });
-//   }
-
-//   try {
-//     // Convert the file to an ArrayBuffer then to a Buffer
-//     const buffer = Buffer.from(await file.arrayBuffer());
-//     const encodedImage = buffer.toString("base64");
-
-//     // Configure the Google Cloud Document AI client
-//     const client = new DocumentProcessorServiceClient();
-//     const projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;
-//     const location = process.env.GOOGLE_CLOUD_LOCATION;
-//     const processorId = process.env.GOOGLE_CLOUD_PROCESSOR_ID;
-//     const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
-
-//     const request = {
-//       name,
-//       rawDocument: {
-//         content: encodedImage,
-//         mimeType: file.type,
-//       },
-//     };
-
-//     // Process the document with Google Cloud Document AI
-//     const [result] = await client.processDocument(request);
-//     const { document } = result;
-//     console.log("The document has been processed.");
-//     console.log(result);
-//     return NextResponse.json({ document });
-//   } catch (error) {
-//     console.error("Failed to process document:", error);
-//     return NextResponse.json({
-//       error: "Failed to process document",
-//       details: (error as Error).message,
-//     });
-//   }
-// };
