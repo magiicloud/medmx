b33a3c4db257d2e1fb85e78f211e25c58f17d11f
magiicloud <hongyun_rc@outlook.com>
2024-05-13 22:15:13 +0800

    chore: update dependencies and configs, install ui components from shadcn and nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(autocomplete|button|input|modal|select|spinner|table|tabs|ripple|listbox|divider|popover|scroll-shadow|checkbox|spacer).js"
   ],
   prefix: "",
   theme: {
