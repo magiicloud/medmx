89ab7d5feb30cdb0babc5a1ee54de7f75ee97b24
magiicloud <hongyun_rc@outlook.com>
2024-05-15 10:28:40 +0800

    fix: amend interface to cater to both UserDrugData and DrugData

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -1,19 +1,21 @@
-export interface DrugData {
+export interface UserDrugData {
   id: number;
   dosingInstruction: string;
-  drug: {
-    id: number;
-    drugName: string;
-    pilLink: string;
-    drugImagesLink: string;
-    counsellingPointsText: string;
-    counsellingPointsVoiceLink: string;
-    otherResources: string;
-    acute: boolean;
-    chronic: boolean;
-    auxInstruction: string;
-    drugClasses: DrugClassData[];
-  };
+  drug: DrugData;
+}
+
+export interface DrugData {
+  id: number;
+  drugName: string;
+  pilLink: string;
+  drugImagesLink: string;
+  counsellingPointsText: string;
+  counsellingPointsVoiceLink: string;
+  otherResources: string;
+  acute: boolean;
+  chronic: boolean;
+  auxInstruction: string;
+  drugClasses: DrugClassData[];
 }
 
 export interface DrugClassData {
