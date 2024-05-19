08ff4f4c9fa90c85e9afd2cfd9378d0b80b8bd07
magiicloud <hongyun_rc@outlook.com>
2024-05-19 15:43:49 +0800

    chore: Install dependencies and update configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(autocomplete|button|input|modal|progress|select|skeleton|spinner|table|tabs|ripple|listbox|divider|popover|scroll-shadow|checkbox|spacer).js",
+    "./node_modules/@nextui-org/theme/dist/components/(autocomplete|button|card|chip|dropdown|input|modal|pagination|progress|select|skeleton|spinner|table|tabs|popover|ripple|listbox|divider|scroll-shadow|menu|checkbox|spacer).js"
   ],
   prefix: "",
   theme: {
