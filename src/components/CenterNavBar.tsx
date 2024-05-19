616c2ef49523b64e4446a4ff5343c6e572b5b7b2
magiicloud <hongyun_rc@outlook.com>
2024-05-20 00:10:22 +0800

    feat: Add AvatarMenu, DropdownItemLink, and update CenterNavBar component
    The code changes add the AvatarMenu component for displaying the user avatar and dropdown menu. It also introduces the DropdownItemLink component and updates the CenterNavBar component to include the new components and improve the navigation menu layout.

--- src/components/CenterNavBar.tsx
+++ src/components/CenterNavBar.tsx
@@ -8,41 +8,87 @@ import {
 } from "@/components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import { useSession } from "next-auth/react";
-import { CustomSession } from "@/auth";
-import { Button } from "@nextui-org/button";
 import Link from "next/link";
+import AvatarMenu from "./AvatarMenu";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+} from "@nextui-org/dropdown";
+import { Button } from "@nextui-org/button";
+import { MenuIcon } from "lucide-react";
 
 export function CenterNavBar() {
+  const { status } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated") return null;
   return (
     <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-6" />
+      <div className="fixed top-10 left-5 max-w-2xl mx-auto z-50 sm:hidden">
+        <DropdownItemLink />
+      </div>
+      <Navbar className="hidden top-6 sm:block" />
+      <div className="fixed top-10 right-5 max-w-2xl mx-auto z-50 sm:right-10">
+        <AvatarMenu />
+      </div>
     </div>
   );
 }
 
+function DropdownItemLink() {
+  return (
+    <Dropdown backdrop="blur">
+      <DropdownTrigger>
+        <Button variant="light" isIconOnly>
+          <MenuIcon />
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu variant="faded" aria-label="Small Screen Nav">
+        <DropdownSection showDivider>
+          <DropdownItem key="home" href="/">
+            Home
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownSection title="Medications" showDivider>
+          <DropdownItem key="medications" href="/medications">
+            My Medication List
+          </DropdownItem>
+          <DropdownItem key="add-medication" href="/add-medication">
+            Add Medication
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownItem key="schedule" href="/schedule">
+          Schedule
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
+
 function Navbar({ className }: { className?: string }) {
-  const { status, data: session } = useSession();
-  const customSession = session as CustomSession | null;
   const [active, setActive] = useState<string | null>(null);
 
-  if (status === "loading") return null;
-  if (status === "unauthenticated") return null;
-
   return (
-    <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
-      <Menu setActive={setActive}>
-        <Link href="/">Home</Link>
-        <MenuItem setActive={setActive} active={active} item="Medications">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/medications">My Medication List</HoveredLink>
-            <HoveredLink href="/add-medication">Add Medication</HoveredLink>
-          </div>
-        </MenuItem>
-        <Link href="/schedule">Schedule</Link>
-        <MenuItem setActive={setActive} active={active} item="Products">
-          {/* <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+    <>
+      <div
+        className={cn(
+          "fixed top-10 inset-x-0 max-w-xs mx-auto z-50 lg:max-w-2xl md:max-w-lg sm:max-w-md",
+          className
+        )}
+      >
+        <Menu setActive={setActive}>
+          <Link href="/">Home</Link>
+          <MenuItem setActive={setActive} active={active} item="Medications">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/medications">My Medication List</HoveredLink>
+              <HoveredLink href="/add-medication">Add Medication</HoveredLink>
+            </div>
+          </MenuItem>
+          <Link href="/schedule">Schedule</Link>
+          <MenuItem setActive={setActive} active={active} item="Products">
+            {/* <div className="  text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Algochurn"
               href="https://algochurn.com"
@@ -68,8 +114,9 @@ function Navbar({ className }: { className?: string }) {
               description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
             />
           </div> */}
-        </MenuItem>
-      </Menu>
-    </div>
+          </MenuItem>
+        </Menu>
+      </div>
+    </>
   );
 }
