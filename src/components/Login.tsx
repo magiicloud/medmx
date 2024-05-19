d79eca4bec77413383a1676515705749300e96a3
magiicloud <hongyun_rc@outlook.com>
2024-05-19 15:33:19 +0800

    refactor: Update Login component UI and add Sign In/Sign Out buttons
    Refactor the Login component to improve UI and user experience. Update the styling to use a gradient background and rounded buttons. Replace the previous buttons with Sign In and Sign Out buttons.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,19 +11,25 @@ const Login = () => {
   const customSession = session as CustomSession;
   return (
     <>
-      <div className="text-center space-x-10 space-y-8 mt-10">
+      <div className="text-center space-y-8 mt-10">
         {status === "authenticated" && (
           <TextGenerateEffect words={`Hello, ${customSession.user!.name}.`} />
         )}
         {status === "authenticated" && (
-          <Button color="primary" size="lg">
-            <Link href="/api/auth/signout">Sign Out</Link>
-          </Button>
+          <button className="p-[3px] relative">
+            <div className="absolute inset-0 bg-gradient-to-r from-[#ff9ff3] via-[#ff6b6b] to-[#1dd1a1] rounded-lg" />
+            <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+              <Link href="/api/auth/signout">Sign Out</Link>
+            </div>
+          </button>
         )}
         {status === "unauthenticated" && (
-          <Button color="primary" size="lg">
-            <Link href="/api/auth/signin">Sign In</Link>
-          </Button>
+          <button className="p-[3px] relative">
+            <div className="absolute inset-0 bg-gradient-to-r from-[#ff9ff3] via-[#ff6b6b] to-[#1dd1a1] rounded-lg" />
+            <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+              <Link href="/api/auth/signin">Sign In</Link>
+            </div>
+          </button>
         )}
       </div>
     </>
