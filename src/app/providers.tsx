08ef832a1317c682ab23b82d0a8d1d1447e8f72d
magiicloud <hongyun_rc@outlook.com>
2024-05-21 11:18:50 +0800

    refactor: Improve query client initialization and management to allow for differentiation between client and server components

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,29 @@ import { NextUIProvider } from "@nextui-org/system";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 
-const queryClient = new QueryClient();
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+const queryClient = getQueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
