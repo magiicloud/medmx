ae4f886f4e85fc81f4b70e84d012b932b658b424
magiicloud <hongyun_rc@outlook.com>
2024-05-22 11:58:24 +0800

    refactor: Handle error when fetching drugs in getAllDrugs function

--- src/app/api/medications/route.ts
+++ src/app/api/medications/route.ts
@@ -2,11 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 export const getAllDrugs = async () => {
-  const drugs = await prisma.drug.findMany();
-  return drugs;
+  try {
+    const drugs = await prisma.drug.findMany();
+    return drugs;
+  } catch (error) {
+    console.error("Error fetching drugs:", error);
+    throw new Error("Error fetching drugs");
+  }
 };
 
 export const GET = async (req: NextRequest) => {
-  const drugs = await getAllDrugs();
-  return NextResponse.json(drugs);
+  try {
+    const drugs = await getAllDrugs();
+    return NextResponse.json(drugs);
+  } catch (error) {
+    console.error("Failed to fetch drugs:", error);
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
 };
