8b53669a9f4440dcf0516382b896a31c5d43dcff
magiicloud <hongyun_rc@outlook.com>
2024-05-26 01:46:16 +0800

    refactor: Add getErrorMessage utility function
    Add a new utility function getErrorMessage to handle error messages.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getErrorMessage = (error: unknown) => {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object") {
+    message = JSON.stringify(error);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "An error occurred";
+  }
+  return message;
+};
