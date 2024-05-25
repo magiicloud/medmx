e6e71615b6d7de086acbc89b43a7ff62f431d0c9
magiicloud <hongyun_rc@outlook.com>
2024-05-26 02:10:02 +0800

    refactor: Move to components folder and update imports

--- src/app/schedule/components/ScheduleCard.tsx
+++ src/app/schedule/components/ScheduleCard.tsx
@@ -4,7 +4,7 @@ import DrugCard from "./DrugCard";
 import { ScheduleData } from "@/types/globalTypes";
 import { SunriseIcon, SunIcon, SunsetIcon, Moon } from "lucide-react";
 import { auth } from "@/auth";
-import { getUserDrugsAndSchedules } from "../actions/getUserDrugsAndSchedules";
+import { getUserDrugsAndSchedules } from "../../actions/getUserDrugsAndSchedules";
 import { redirect } from "next/navigation";
 
 const ScheduleCard = async () => {
--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ScheduleCard from "./ScheduleCard";
+import ScheduleCard from "./components/ScheduleCard";
 import { MotionDiv } from "@/components/MotionDiv";
 
 const Schedule = () => {
