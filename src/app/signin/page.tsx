80a2a243b9e198f02c62f07ee87f534e5d99fc69
magiicloud <hongyun_rc@outlook.com>
2024-05-26 01:54:50 +0800

    feat: Add sign-in page with social media authentication
    This commit adds a new sign-in page component that allows users to sign in using social media authentication providers.

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,81 @@
+import { providerMap, signIn, auth } from "@/auth";
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
+import { Button } from "@/components/ui/button";
+import { MotionDiv } from "@/components/MotionDiv";
+
+export default async function SignInPage() {
+  return (
+    <div className="flex justify-center items-center h-full opacity-80">
+      <MotionDiv>
+        <Card className="w-96 p-6">
+          <CardHeader className="justify-center text-3xl font-bold">
+            <p className="text-center">Sign In</p>
+          </CardHeader>
+          <CardBody className="space-y-4">
+            {Object.values(providerMap).map((provider) => (
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn(provider.id, { redirectTo: "/" });
+                }}
+              >
+                <Button type="submit" className="w-full" variant="outline">
+                  {provider.name === "Google" && (
+                    <ChromeIcon className="w-5 h-5 mr-2" />
+                  )}
+                  {provider.name === "GitHub" && (
+                    <GithubIcon className="w-5 h-5 mr-2" />
+                  )}
+                  <span>Continue with {provider.name}</span>
+                </Button>
+              </form>
+            ))}
+          </CardBody>
+        </Card>
+      </MotionDiv>
+    </div>
+  );
+}
+
+function ChromeIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <circle cx="12" cy="12" r="4" />
+      <line x1="21.17" x2="12" y1="8" y2="8" />
+      <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
+      <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
+    </svg>
+  );
+}
+
+function GithubIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+      <path d="M9 18c-4.51 2-5-2-7-2" />
+    </svg>
+  );
+}
