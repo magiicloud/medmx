70951b1ee38cfa538e5e2c214b9cac0d6fc07187
magiicloud <hongyun_rc@outlook.com>
2024-05-26 16:27:47 +0800

    feat: Update Home component with new design and MedicationsNow integration

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,14 @@
-"use client";
 import React from "react";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import { auth } from "@/auth";
 
-const Login = () => {
-  const { status, data: session } = useSession();
+const Login = async () => {
+  const session = await auth();
 
   return (
     <>
       <div className="text-center space-y-8 mt-10 ">
-        {status === "authenticated" && (
-          <TextGenerateEffect words={`Hello, ${session.user!.name}.`} />
-        )}
-
-        {status === "unauthenticated" && (
+        {!session && (
           <button className="p-[3px] relative">
             <div className="absolute inset-0 bg-gradient-to-r from-[#ff9ff3] via-[#ff6b6b] to-[#1dd1a1] rounded-lg" />
             <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
